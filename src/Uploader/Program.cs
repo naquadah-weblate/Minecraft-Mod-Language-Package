6a9da1f1d4940b1ccc22934f1f6962f22550002d
NostalgiaCyan <cyl18a@gmail.com>
2022-02-16 19:13:15 +0800

    Update Program.cs

--- src/Uploader/Program.cs
+++ src/Uploader/Program.cs
@@ -39,7 +39,7 @@ namespace Uploader
 
             command.Handler = CommandHandler.Create<string, string, string, string>((version, host, name, password) =>
             {
-                using var scpClient = new ScpClient(host, 20002, name, password);
+                using var scpClient = new ScpClient(host, 22, name, password);
                 scpClient.Connect();
                 if (scpClient.IsConnected)
                 {
@@ -52,25 +52,25 @@ namespace Uploader
                 }
 
                 var md5s = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.md5");
-                md5s.ToList().ForEach(_ => { scpClient.Upload(File.OpenRead(_), $"/var/www/html/files/{Path.GetFileName(_)}"); });
+                md5s.ToList().ForEach(_ => { scpClient.Upload(File.OpenRead(_), $"/www/{Path.GetFileName(_)}"); });
                 var fs = File.OpenRead($"./Minecraft-Mod-Language-Package-{version}.zip");
                 switch (version)
                 {
                     case "1.12.2":
-                        scpClient.Upload(fs, "/var/www/html/files/Minecraft-Mod-Language-Modpack.zip.1");
+                        scpClient.Upload(fs, "/www/Minecraft-Mod-Language-Modpack.zip.1");
                         break;
                     case "1.16":
-                        scpClient.Upload(fs, "/var/www/html/files/Minecraft-Mod-Language-Modpack-1-16.zip.1");
+                        scpClient.Upload(fs, "/www/Minecraft-Mod-Language-Modpack-1-16.zip.1");
                         break;
                     case "1.18":
-                        scpClient.Upload(fs, "/var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip.1");
+                        scpClient.Upload(fs, "/www/Minecraft-Mod-Language-Modpack-1-18.zip.1");
                         break;
                     default:
                         break;//不应该
                 }
                 Log.Logger.Information("上传成功");
                 scpClient.Dispose();
-                using var sshClient = new SshClient(host, 20002, name, password);
+                using var sshClient = new SshClient(host, 2, name, password);
                 sshClient.Connect();
                 if (sshClient.IsConnected)
                 {
@@ -84,19 +84,19 @@ namespace Uploader
                 switch (version)
                 {
                     case "1.12.2":
-                        var cmd1 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack.zip");
+                        var cmd1 = sshClient.CreateCommand("mv /www/Minecraft-Mod-Language-Modpack.zip.1 /www/Minecraft-Mod-Language-Modpack.zip");
                         cmd1.Execute();
                         var err1 = cmd1.Error;
                         Log.Logger.Error(err1);
                         break;
                     case "1.16":
-                        var cmd2 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-16.zip");
+                        var cmd2 = sshClient.CreateCommand("mv /www/Minecraft-Mod-Language-Modpack-1-16.zip.1 /www/Minecraft-Mod-Language-Modpack-1-16.zip");
                         cmd2.Execute();
                         var err2 = cmd2.Error;
                         Log.Logger.Error(err2);
                         break;
                     case "1.18":
-                        var cmd3 = sshClient.CreateCommand("mv /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip.1 /var/www/html/files/Minecraft-Mod-Language-Modpack-1-18.zip");
+                        var cmd3 = sshClient.CreateCommand("mv /www/Minecraft-Mod-Language-Modpack-1-18.zip.1 /www/Minecraft-Mod-Language-Modpack-1-18.zip");
                         cmd3.Execute();
                         var err3 = cmd3.Error;
                         Log.Logger.Error(err3);
